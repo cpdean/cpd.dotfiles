92a5c53f9bff2d74d725e959dab2d03bd76a1833
Conrad Dean <conrad.p.dean@gmail.com>
2020-10-22 21:32:14 -0400

    add default reset sequence to idempotently cancel out of pagers

--- custom-scripts/python_kitty_chunked_send.py
+++ custom-scripts/python_kitty_chunked_send.py
@@ -3,9 +3,18 @@ import sys
 import io
 import subprocess
 
+# q and ctrl-u
+default_reset = "q\025"
+
 # warning: this buffers all of stdin into memory
 lines = sys.stdin.readlines()
 batch_size = 40
+
+kitty = subprocess.run(
+    ["kitty", "@", "send-text", "--match=num:1", "--stdin"],
+    input=default_reset,
+    encoding='ascii'
+)
 
 for i in range(0, len(lines), batch_size):
     batch = lines[i:i+batch_size]
