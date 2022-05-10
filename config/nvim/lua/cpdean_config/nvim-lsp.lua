c3b7c9b732fb2c975be9d6f514e942ce75cf7dfb
Conrad Dean <conrad.p.dean@gmail.com>
2022-05-10 10:00:51 -0400

    disabling the codeaction plugin from lsputil. somethin broke

--- config/nvim/lua/cpdean_config/nvim-lsp.lua
+++ config/nvim/lua/cpdean_config/nvim-lsp.lua
@@ -374,6 +374,6 @@ else
 end
 
 -- from RishabhRD/nvim-lsputils
-vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
+-- vim.lsp.handlers['textDocument/codeAction'] = require'lsputil.codeAction'.code_action_handler
 
 
