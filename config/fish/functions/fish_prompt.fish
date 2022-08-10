b877df2c3097badba4211a9c6bac9c77e5bca509
Conrad Dean <conrad.p.dean@gmail.com>
2022-08-10 07:54:00 -0400

    add error code to prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,12 @@
 function fish_prompt -d "Write out the prompt"
-    # '~/.dotfiles > '
-    printf '%s%s%s > ' (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    # need to save this before we run any other commands in here
+    set previous_command_status $status
+    if test "$previous_command_status" != "0"
+    printf '%s%s%s %s%s%s > ' \
+        (set_color $fish_color_status) $previous_command_status (set_color normal) \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    else
+    printf '%s%s%s > ' \
+        (set_color $fish_color_cwd) (prompt_pwd) (set_color normal)
+    end
 end
