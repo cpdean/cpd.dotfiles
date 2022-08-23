5159e8f4b710461b308135ff0d464eb72c08df47
Conrad Dean <conrad.p.dean@gmail.com>
2022-08-23 12:30:59 -0400

    impl cache

--- custom-scripts/my-jira.sh
+++ custom-scripts/my-jira.sh
@@ -3,10 +3,23 @@ source $HOME/.jirapass
 
 QUERY=jql=not+%28status+%3D++Done%29+AND+assignee+in+%28$USER_ID%29
 
-curl \
+CACHE=~/.my-jira-cache
+if [[ -f  $CACHE ]]; then
+    cat $CACHE
+else
+    curl \
+        -s \
+        -u $USER:$TOKEN \
+        -X GET \
+        -H "Content-Type: application/json" \
+        https://$HOST/rest/api/2/search?$QUERY |
+        jq -r '.issues[] | "\(.key)\tJIRA: \(.fields.summary)"'
+fi
+
+$(curl \
     -s \
     -u $USER:$TOKEN \
     -X GET \
     -H "Content-Type: application/json" \
     https://$HOST/rest/api/2/search?$QUERY |
-    jq -r '.issues[] | "\(.key)\tJIRA: \(.fields.summary)"'
+    jq -r '.issues[] | "\(.key)\tJIRA: \(.fields.summary)"' > ~/.my-jira-cache &)
