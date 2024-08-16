f85d9d73605577db9060ae6eab210a545bc18de9
Conrad Dean <conrad.p.dean@gmail.com>
2024-08-16 11:27:05 -0400

    update some vars and document them

--- custom-scripts/my-jira.sh
+++ custom-scripts/my-jira.sh
@@ -1,7 +1,14 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -e
+
+# file in the form of
+#JIRA_HOST=yourcompany.atlassian.net
+#JIRA_TOKEN=token # mint one at https://id.atlassian.com/manage-profile/security/api-tokens
+#JIRA_USER=jira email i think
+# find userid by using the query builder and then switch to raw. then look at the query parms in the url
+#JIRA_USER_ID=some number
 source $HOME/.jirapass
 
-RAW_QUERY="assignee in ($USER_ID) and status != \"Completed\" and status != \"Closed\" and status != \"Done\" AND status != \"Will not do\" AND created >= -150d order by created DESC"
+RAW_QUERY="assignee in ($JIRA_USER_ID) and status != \"Completed\" and status != \"Closed\" and status != \"Done\" AND status != \"Will not do\" AND created >= -150d order by created DESC"
 
 QUERY=$(echo "${RAW_QUERY}" | python3 -c "import sys, urllib.parse as p; print(p.urlencode({'jql': sys.stdin.read().strip()}))")
 
@@ -11,17 +18,17 @@ if [[ -f  $CACHE ]]; then
 else
     curl \
         -s \
-        -u $USER:$TOKEN \
+        -u $JIRA_USER:$JIRA_TOKEN \
         -X GET \
         -H "Content-Type: application/json" \
-        https://$HOST/rest/api/2/search?$QUERY |
+        https://$JIRA_HOST/rest/api/2/search?$QUERY |
         jq -r '.issues[] | "\(.key)\tJIRA: \(.fields.summary)"'
 fi
 
 $(curl \
     -s \
-    -u $USER:$TOKEN \
+    -u $JIRA_USER:$JIRA_TOKEN \
     -X GET \
     -H "Content-Type: application/json" \
-    https://$HOST/rest/api/2/search?$QUERY |
+    https://$JIRA_HOST/rest/api/2/search?$QUERY |
     jq -r '.issues[] | "\(.key)\tJIRA: \(.fields.summary)"' > ~/.my-jira-cache &)
