266396d65fd1a700f3bbd397618a46b02d2d4989
Conrad Dean <github@conradpdean.com>
2026-06-15 19:01:44 -0400

    disable leap.nvim for now
    the github repo moved to codeberg and add_default_mappings is deprecated;
    commenting the spec out silences both warnings until it's re-added with the new
    url + mapping api.

--- config/nvim/lua/cpdean_config/plugins/editing.lua
+++ config/nvim/lua/cpdean_config/plugins/editing.lua
@@ -2,13 +2,15 @@
 -- loaded eagerly to match vim-plug behavior.
 return {
   { "tpope/vim-surround", lazy = false },
-  {
-    "ggandor/leap.nvim",
-    lazy = false,
-    config = function()
-      require("leap").add_default_mappings()
-    end,
-  },
+  -- leap disabled for now: the repo moved to Codeberg and add_default_mappings
+  -- is deprecated. re-enable with the new url + mapping API when ready.
+  -- {
+  --   "ggandor/leap.nvim",
+  --   lazy = false,
+  --   config = function()
+  --     require("leap").add_default_mappings()
+  --   end,
+  -- },
   -- structural editing for lisps (fennel, janet, hy, ...)
   { "gpanders/nvim-parinfer", lazy = false },
 }
