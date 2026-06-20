e64cec4e54d814b1c582bfd3821a321261cd7405
Conrad Dean <github@conradpdean.com>
2026-06-20 11:25:55 -0400

    switch avante lmstudio model to qwen3.5-4b

--- config/nvim/lua/cpdean_config/plugins/avante.lua
+++ config/nvim/lua/cpdean_config/plugins/avante.lua
@@ -20,7 +20,7 @@ return {
         __inherited_from = "openai",
         api_key_name = "",
         endpoint = "http://127.0.0.1:1234/v1",
-        model = "qwen/qwen3.5-9b",
+        model = "qwen3.5-4b",
       },
     },
   },
